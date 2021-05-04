69ace9d71dd815ea8a66f55ad73562105f8fc69d
Elijah Prom <78039979+S2Mackinley@users.noreply.github.com>
2021-05-04 09:40:04 -0700

    Update userSchema.js

--- src/auth/routes/userSchema.js
+++ src/auth/routes/userSchema.js
@@ -8,10 +8,6 @@ const basicAuth = require('../middleware/basicAuth.js');
 
 const router = express.Router();
 
-// Signup Route -- create a new user
-// Two ways to test this route with httpie
-// echo '{"username":"john","password":"foo"}' | http post :3000/signup
-// http post :3000/signup usernmae=john password=foo
 router.post('/signup', async (req, res, next) => {
 	try {
 		req.body.password = await bcrypt.hash(req.body.password, 5);
@@ -23,9 +19,7 @@ router.post('/signup', async (req, res, next) => {
 	}
 });
 
-// Signin Route -- login with username and password
-// test with httpie
-// http post :3000/signin -a john:foo
+
 router.post('/signin', basicAuth, async (req, res, next) => {
 	res.status(200).json(req.user);
 });
